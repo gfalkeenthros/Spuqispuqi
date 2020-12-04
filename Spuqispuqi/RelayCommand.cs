9f079f8d27cec8d7ca1e9eebc90b4bee1321d891
gstok123 <gstok123@gmail.com>
2020-12-04 14:00:40 +0100

    Add project files.

---
+++ Spuqispuqi/RelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Spuqispuqi
+{
+    public class RelayCommand : ICommand
+    {
+        protected Action _execute = () => { };
+        protected Func<bool> _canExecute = () => true;
+
+        public RelayCommand() { }
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("Execute");
+            }
+
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("CanExecute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public virtual bool CanExecute(object parameter = null)
+        {
+            return _canExecute();
+        }
+
+        public virtual void Execute(object parameter = null)
+        {
+            _execute();
+        }
+
+        public bool IsEnabled { get => CanExecute(); }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Spuqispuqi/Viewmodel/TestViewmodel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spuqispuqi.Viewmodel
+{
+    class TestViewmodel : ViewModelBase
+    {
+        private DateTimeOffset dateFrom1;
+        private TimeSpan timeFrom1;
+
+        public DateTimeOffset dateFrom
+        {
+            get => dateFrom1;
+            set
+            {
+                dateFrom1 = value;
+                OnPropertyChanged(nameof(dateFrom));
+            }
+        }
+        public TimeSpan timeFrom
+        {
+            get => timeFrom1;
+            set
+            {
+                timeFrom1 = value;
+                OnPropertyChanged(nameof(timeFrom));
+            }
+        }
+
+        public DateTimeOffset dateAndTime { get; set; }
+        public String Combined { get; set; }
+
+        public RelayCommand Combine { get; set; }
+
+        public TestViewmodel()
+        {
+            dateFrom = DateTime.Today;
+            timeFrom = DateTime.Now.TimeOfDay;
+            Combine = new RelayCommand(CombineDates);
+            Combined = "No date picked yet";
+        }
+
+        private void CombineDates()
+        {
+            dateAndTime = dateFrom.Date + timeFrom;
+            Combined = dateAndTime.ToString("dd/MMx/yyyy HH:mm:tt");
+            OnPropertyChanged(nameof(Combined));
+        }
+    }
+}
